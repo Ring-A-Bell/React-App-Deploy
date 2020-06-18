c957a4019dd473fb4562a1c6c4b0bac9308577ac
Aditya Ganti <aditya.ganti2018@vitstudent.ac.in>
2020-06-18 20:07:55 +0530

    Updates

---
+++ precache-manifest.f3447d032a9117ba8422495832cfc66a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39777f9f8a293c4dfdcd98878818c60b",
+    "url": "/React-App-Deploy/index.html"
+  },
+  {
+    "revision": "3d444230fea86b5d1271",
+    "url": "/React-App-Deploy/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b641a0d46e428f84bb5d",
+    "url": "/React-App-Deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3d444230fea86b5d1271",
+    "url": "/React-App-Deploy/static/js/2.ae8fcd8d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-App-Deploy/static/js/2.ae8fcd8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b641a0d46e428f84bb5d",
+    "url": "/React-App-Deploy/static/js/main.5ff0114b.chunk.js"
+  },
+  {
+    "revision": "941765526e895bb5b82e",
+    "url": "/React-App-Deploy/static/js/runtime-main.8a953eee.js"
+  }
+]);
